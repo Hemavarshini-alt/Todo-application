97795c962d444074dbc975485e041f611cdf5448
Hemavarshini-alt <hemavarshi218@gmail.com>
2026-02-20 11:17:04 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const Todo = require("./model/Todo");
+const Todo = require("./model/todo");
 
 const app = express();
 
