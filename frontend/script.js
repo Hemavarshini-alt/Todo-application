abded7fdeedce4cad29ba09862631721480ed145
Hemavarshini-alt <hemavarshi218@gmail.com>
2026-02-23 11:43:57 +0530

    FE
    Signed-off-by: Hemavarshini-alt <hemavarshi218@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ const completedSpan = document.getElementById("completed");
 const themeToggle = document.getElementById("theme-toggle");
 
 // ✅ Add /todolist endpoint
-const API_URL = "http://localhost:3000/todolist";
+const API_URL = "https://todo-application-backend-dvt7.onrender.com/todolist";
 
 // function to update stats
 function updateStats() {
