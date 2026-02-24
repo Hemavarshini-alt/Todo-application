70ee4e6a3385acf7ae6f76a9deb3d2189e3269b9
Hemavarshini-alt <hemavarshi218@gmail.com>
2026-02-24 11:28:08 +0530

    fs
    Signed-off-by: Hemavarshini-alt <hemavarshi218@gmail.com>

---
+++ Todo-application/src/App.jsx
@@ -0,0 +1,19 @@
+function Hello(){
+  return (
+    <>
+    <h1>todolist</h1>
+    <h2>new task</h2>
+    </>
+  );
+}
+
+function Task(){
+  return (
+    <>
+    
+     <p>entry your task</p>
+      <Hello/>
+    </>
+  );
+}
+export default Task;
